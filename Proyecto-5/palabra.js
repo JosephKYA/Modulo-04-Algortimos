f5c8045b8c76363416ecd235286cfff1fe5a21d9
JosephKYA <josephcv2005@gmail.com>
2026-08-19 16:56:33 -0600

    Proyecto-5 Palabra más larga

---
+++ Proyecto-5/palabra.js
@@ -0,0 +1,18 @@
+function findLongestWord(text) {
+    const words = text.split(' ');
+
+    let longestWord = '';
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+
+    return longestWord;
+}
+
+// Ejemplo de uso
+const text = "JavaScript es un lenguaje de programación increíble para aprender.";
+
+console.log(findLongestWord(text)); // Resultado esperado: "programación"
